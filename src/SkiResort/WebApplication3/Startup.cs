6ae2e668904763b9a770cd38d58d15c89576891a
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2022-10-12 19:19:45 +0300

    new 7 task

---
+++ src/SkiResort/WebApplication3/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System;
+using Microsoft.AspNetCore.SpaServices;
+using AutoMapper;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+			//services.AddMemoryCache();
+			//services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			//services.AddReact();
+			//services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName).AddChakraCore();
+
+			services.AddMvc();
+			//services.AddAutoMapper();
+		}
+
+		public void Configure(IApplicationBuilder app, Microsoft.Extensions.Hosting.IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseWebpackDevMiddleware(); // 1
+			}
+
+			app.UseStaticFiles();
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "DefaultApi",
+					template: "api/{controller}/{action}");
+				routes.MapSpaFallbackRoute("spa-fallback", new { controller = "Home", action = "Index" }); // 2
+			});
+		}
+	}
+}
---
+++ src/SkiResort/WebApplication3/webpack.config.js
@@ -0,0 +1,34 @@
+﻿//руководствуясь которым, webpack будет применить различные препроцессоры и собирать наше приложение.
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+const bundleFolder = "./wwwroot/assets/";
+const srcFolder = "./App/"
+
+module.exports = {
+    entry: [
+        srcFolder + "index.jsx" //точка входа webpack в наши исходники 
+    ],
+    devtool: "source-map",
+    output: {
+        filename: "bundle.js",
+        publicPath: 'assets/',
+        path: path.resolve(__dirname, bundleFolder)
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["es2015", "stage-0", "react"]
+                }
+            }
+        ]
+    },
+    plugins: [
+    ]
+};
